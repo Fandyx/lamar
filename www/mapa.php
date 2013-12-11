4eaa5a18c28ad11531db912263e4d325eea7de66
unknown <DiegoAndres@.(none)>
2013-12-11 00:49:50 -0500

    mapas

--- www/mapa.php
+++ www/mapa.php
@@ -8,9 +8,23 @@ include 'header.php';
 ?>
 <div id="content">
 	
+	<p>&nbsp;</p>
+	<div id="mapp">
+		<p>&nbsp;</p>
+		<h2>Ubicado en la Carrera 42 H # 84B-41 de Barranquilla, 
+			con excelentes vías de acceso, Dos (2) ascensores, 
+			escaleras contra-incendio, tanques de reserva de agua, 
+			planta eléctrica de suplencia Total. Servicio Públicos Estrato 4.
+		</h2>
+	</div>
+	<div id="map">
+		<img src="images/mapa.png">
+	</div>
+	<p>&nbsp;</p>
 </div>
 <?php
 include 'footer.php';
 ?>
 </body>
+
 </html>
