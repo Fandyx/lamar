c9a9e11bb771630401aaf05eaa669849915683c9
unknown <DiegoAndres@.(none)>
2013-12-11 01:16:04 -0500

    Creacion

--- www/lacreacion.php
+++ www/lacreacion.php
@@ -7,7 +7,24 @@
 include 'header.php';
 ?>
 <div id="content">
+	<p>&nbsp;</p>
 	
+	<div id="mapp">
+		<p>&nbsp;</p>
+		<h2>Nuestra Creación</h2>
+		<p>La mar es un edificio de 12 pisos con apartamentos desde 82,3 hasta 131,6 m2 de construcción, además de amplios penthouses.
+		</p>
+		<p>Ubicado en la Carrera 42 H # 84B-41 de Barranquilla, 
+			con excelentes vías de acceso, Dos (2) ascensores, 
+			escaleras contra-incendio, tanques de reserva de agua, 
+			planta eléctrica de suplencia Total. Servicio Públicos Estrato 4.
+		</p>
+	</div>
+	<div id="map">
+		
+		<img src="images/facha2.png">
+	</div>
+	<p>&nbsp;</p>
 </div>
 <?php
 include 'footer.php';
